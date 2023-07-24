fdb6113766e5c5a8a05bcd753c5b4e7d06a8c051
tumee <tmkee0525@gmail.com>
2023-07-24 21:13:59 +0900

    icon add

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -6,6 +6,7 @@
 <link rel="icon" type="image/png" href="images/favicon.png">
 <!-- Web Font -->
 <link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
 <!-- StyleSheet -->
 <link rel="manifest" href="/manifest.json">
